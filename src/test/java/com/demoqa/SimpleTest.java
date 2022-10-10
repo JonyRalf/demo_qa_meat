818cc857984e37aea0efde94fcc7ac1ffa703020
JonyRalf <ralfsntrenje@gmail.com>
2022-10-10 18:47:37 +0300

    Junit 5

---
+++ src/test/java/com/demoqa/SimpleTest.java
@@ -0,0 +1,45 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+public class SimpleTest {
+    @BeforeAll
+    static void clear () {
+        System.out.println("### BeforeAll");
+    }
+    @BeforeEach
+    void assertStart () {
+        System.out.println("### BeforeEach");
+        Selenide.open("https://demoqa.com/");
+    }
+    @AfterEach
+    void assertEnd (){
+        System.out.println("### BeforEach");
+        WebDriverRunner.closeWindow();
+    }
+    @BeforeAll
+    static void clearOut () {
+        System.out.println("### BeforeAll_out");
+    }
+    @Test
+    void assertTest1 () {
+        System.out.println("### Test_1");
+    }
+    @Test
+    void assertTest2 () {
+        System.out.println("### Test_2");
+    }
+    @Test
+    void assertTest3 () {
+        System.out.println("### Test_3");
+    }
+
+    @Test
+    void assertTest4 () {
+        System.out.println("### Test_4");
+    }
+
+
+}
